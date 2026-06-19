a1a463f1f8b9719712caf57fe8c03f29bc6453a6
Ebranur Özbalık <98876341+EbranurOzbalik@users.noreply.github.com>
2026-06-19 23:45:15 +0300

    Day 2 connect Firebase Android

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'constants/app_constants.dart';
-import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
 
   runApp(const OrmanPazarApp());
 }
